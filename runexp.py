59d4f91154e0285a7b76e71cf1031c54389a7d10
Dongqin Zhou <dongqinzhou1110@gmail.com>
2020-05-18 20:10:00 -0400

    colight frap, state reward pressure

--- runexp.py
+++ runexp.py
@@ -22,7 +22,7 @@ ANON_PHASE_REPRE=[]
 def parse_args():
     parser = argparse.ArgumentParser()
     # The file folder to create/log in
-    parser.add_argument("--memo", type=str, default='colight_FRAP_reward_pressure_state_lane_num_vehicle')
+    parser.add_argument("--memo", type=str, default='colight_FRAP_reward_pressure_state_pressure')
     parser.add_argument("--env", type=int, default=1)  # env=1 means you will run CityFlow
     parser.add_argument("--gui", type=bool, default=False)
     parser.add_argument("--road_net", type=str, default='3_3')  # which road net you are going to run
@@ -255,7 +255,7 @@ def main(memo, env, road_net, gui, volume, suffix, mod, cnt, gen, r_all, workers
                 # "vehicle_speed_img",
                 # "vehicle_acceleration_img",
                 # "vehicle_waiting_time_img",
-                "lane_num_vehicle",
+                # "lane_num_vehicle",
                 # "lane_num_vehicle_been_stopped_thres01",
                 # "lane_num_vehicle_been_stopped_thres1",
                 # "lane_queue_length",
@@ -266,7 +266,7 @@ def main(memo, env, road_net, gui, volume, suffix, mod, cnt, gen, r_all, workers
                 # "coming_vehicle",
                 # "leaving_vehicle",
                 "pressure"
-                # "pressure_of_movement",
+                "pressure_of_movement",
                 # "adjacency_matrix",
                 # "lane_queue_length",
                 # "connectivity",
