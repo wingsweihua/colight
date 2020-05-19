ef3d63f244844fad9ca78040a15501d56a0b0dbb
DongqinZhou <dongqinzhou1110@gmail.com>
2020-05-19 11:58:43 -0400

    use lane num vehicle to debug

--- summary_multi_anon.py
+++ summary_multi_anon.py
@@ -20,7 +20,7 @@ NAN_LABEL = -1
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--memo", type=str, default='colight_8_phase_reward_queue_length_state_pressure')
+    parser.add_argument("--memo", type=str, default='colight_FRAP_reward_queue_length_state_lane_num_vehicle')
     parser.add_argument("--state", type=str, default='pressure_of_movement')
     parser.add_argument("-b", action="store_true", default=False, help="run baseline analysis")
 
